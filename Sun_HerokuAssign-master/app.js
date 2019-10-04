1a1b754a8fd2c0d7bf5cb80db08a7a1f191f1d60
unscientifi <a_sun69708@fanshaweonline.ca>
2019-10-04 15:16:50 -0400

    responsive modified

--- Sun_HerokuAssign-master/app.js
+++ Sun_HerokuAssign-master/app.js
@@ -8,10 +8,19 @@ app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 
+
+app.set('views', __dirname + '/views');
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/index.html"));
+    console.log('at the home route');
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/about.html"));
+    console.log('at the about route');
 })
 
 app.listen(port, () => {
-    console.log(`app is running on ${port}`);
+    console.log(`app is running on ${port}/`);
 })
